df3750dccd5e25855496e647cb459720890fe8db
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-02-22 14:30:30 +0300

    add print statement to print all the possible generators

--- main.py
+++ main.py
@@ -6,14 +6,19 @@ for i in range(0,prime-1):
 
 gen_list = []
 
+possible_gen = []
+
 for i in range(2,prime):
     for j in range(1, len(list)+1):
         ins = i**j % prime
         gen_list.append(ins)
     if sorted(gen_list) == list:
         print("The generator" , i ,"produces all the elements of the main list")
+        possible_gen.append(i)
         print(gen_list)
         gen_list.clear()
     else:
         gen_list.clear()
         continue
+
+print("\n\nThe possible generators are: ", possible_gen)
