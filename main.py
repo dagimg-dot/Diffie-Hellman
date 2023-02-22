3812b0ab65ace0d0107de5a30824c26befe91820
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-02-22 14:15:23 +0300

    add main.py file

---
+++ main.py
@@ -0,0 +1,19 @@
+prime = int(input("Enter the prime number: "))
+
+list = []
+for i in range(0,prime-1):
+    list.append(i+1)
+
+gen_list = []
+
+for i in range(2,prime):
+    for j in range(1, len(list)+1):
+        ins = i**j % prime
+        gen_list.append(ins)
+    if sorted(gen_list) == list:
+        print(f"The generator" , i ,"produces all the elements of the main list")
+        print(gen_list)
+        gen_list.clear()
+    else:
+        gen_list.clear()
+        continue
